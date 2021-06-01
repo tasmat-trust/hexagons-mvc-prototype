dd14c135cb9d8cf755e7cfe0758d25aa5f3dabac
Ali Blackwell <aliblackwell@protonmail.com>
2021-06-01 17:41:08 +0100

    Add health check

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,16 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
+})
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy
+    ? response.ok(report)
+    : response.badRequest(report)
 })
